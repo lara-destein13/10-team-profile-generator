abdfe60435b55962c756ae1dfcc7acd24dff0c85
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-01-07 17:53:32 -0500

    use inquirer to get properties for all three employee types

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const Employee = require('./src/Employee');
+const Engineer = require('./src/Engineer');
+const Intern = require('./src/Intern');
+const Manager = require('./src/Manager');
 const inquirer = require('inquirer');
 
 class Main {
+
+  constructor() {
+    this.employees = [];
+  }
+  
   async run() {
 
-    while (true) {
+    let done = false;
+
+    while (!done) {
       const employeeType = await this.getEmployeeType();
       switch (employeeType) {
         case 'Done':
-          return;
+          done = true;
+          break;
         case 'Engineer':
           console.log('She is an Engineer');
+          await this.getEngineer();
           break;
         case 'Intern':
           console.log('She is an Intern');
+          await this.getIntern();
           break;
-        case 'Intern':
+        case 'Manager':
           console.log('She is a Manager');
+          await this.getManager();
           break;
         default:      
           console.log('No such employee type');
+          return;
       }
     }
+
+    console.dir(this.employees);
   }
 
   async getEmployeeType() {
@@ -46,6 +63,135 @@ class Main {
 
     const answers = await promise;
     return answers['type'];
+  }
+
+  async getEngineer() {
+    const questions = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name: ',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Id: ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Email: ',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Github: ',
+      },
+    ];
+    
+    const promise = new Promise((resolve) => {
+      inquirer.prompt(questions)
+        .then((answers) => {
+          resolve(answers);
+        })
+        .catch((error) => {
+          console.log(`error: ${error}`);
+        });
+    });
+
+    const answers = await promise;
+    const email = answers['email'];
+    const github = answers['github'];
+    const id = answers['id'];
+    const name = answers['name'];
+    const engineer = new Engineer(name, id, email, github);
+    this.employees.push(engineer);
+  }
+
+  async getIntern() {
+    const questions = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name: ',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Id: ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Email: ',
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: 'School: ',
+      },
+    ];
+    
+    const promise = new Promise((resolve) => {
+      inquirer.prompt(questions)
+        .then((answers) => {
+          resolve(answers);
+        })
+        .catch((error) => {
+          console.log(`error: ${error}`);
+        });
+    });
+
+    const answers = await promise;
+    const email = answers['email'];
+    const school = answers['school'];
+    const id = answers['id'];
+    const name = answers['name'];
+    const intern = new Intern(name, id, email, school);
+    this.employees.push(intern);
+  }
+
+  async getManager() {
+    const questions = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name: ',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Id: ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Email: ',
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Office Number: ',
+      },
+    ];
+    
+    const promise = new Promise((resolve) => {
+      inquirer.prompt(questions)
+        .then((answers) => {
+          resolve(answers);
+        })
+        .catch((error) => {
+          console.log(`error: ${error}`);
+        });
+    });
+
+    const answers = await promise;
+    const email = answers['email'];
+    const officeNumber = answers['officeNumber'];
+    const id = answers['id'];
+    const name = answers['name'];
+    const manager = new Manager(name, id, email, officeNumber);
+    this.employees.push(manager);
   }
 }
 
